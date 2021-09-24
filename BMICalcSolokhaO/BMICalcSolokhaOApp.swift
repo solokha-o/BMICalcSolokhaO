d892432ef845774f5993dcd40f36223b1b7b3740
Oleksandr Solokha <oleksandr.solokha@gmail.com>
2021-09-24 18:09:13 +0300

    Initial Commit

---
+++ BMICalcSolokhaO/BMICalcSolokhaOApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalcSolokhaOApp.swift
+//  BMICalcSolokhaO
+//
+//  Created by Oleksandr Solokha on 24.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct BMICalcSolokhaOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
